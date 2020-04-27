19950f7e8277bf75bc9ebb5308fb8edf783c4415
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2020-04-27 21:46:03 +0200

    Add command to create includes for nano syntax highlighting

--- init.sh
+++ init.sh
@@ -15,6 +15,10 @@ stow -R --adopt zsh
 echo "Reset git repo"
 git reset --hard
 
+echo "Add includes for nano syntax highlighting"
+echo "\n" >> ~/.nanorc
+find /usr/share/nano/ -iname "*.nanorc" -exec echo include {} \; | sort >> ~/.nanorc
+
 echo "Changing permissions for .shh"
 chmod 644 ~/.ssh/config
 chmod 644 ~/.ssh/authorized_keys
